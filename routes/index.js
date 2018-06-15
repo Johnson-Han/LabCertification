ba8b707a54c9a86b83fad6a1b0a4d6d34ee6bb71
Johnson-Han <hanlifeng@sinap.ac.cn>
2018-06-15 16:21:34 +0800

    修改sql语言

--- routes/index.js
+++ routes/index.js
@@ -107,14 +107,12 @@ router.post('/lf_contract_add', multer({ storage: storage1 }).single('file'), fu
 
   var upftime = newDate.toISOString();
 
-  sql = 'insert into  lab_files_subject (subject,creator,create_date) values (\'' + req.body.subject + '\',\'' + req.body.creator + '\',\''   + upftime + '\')';
-
-  sql1 = 'insert into  cer_files (filename,file_addr,upload_date,cer_index,subject) values (\'' + req.file.filename + '\',\'' + filepath + '\',\'' + upftime + '\',\'' + req.body.cer_index + '\',\''+req.body.subject+'\')';
+  sql = 'insert into  lab_files_subject (subject,creator,create_date) values (\'' + req.body.subject + '\',\'' + req.body.creator + '\',\''   + upftime + '\') ; insert into  cer_files (filename,file_addr,upload_date,cer_index,subject) values (\'' + req.file.filename + '\',\'' + filepath + '\',\'' + upftime + '\',\'' + req.body.cer_index + '\',\''+req.body.subject+'\');';
 
   console.log(sql);
-  console.log(sql1);
 
-  pg2.query(sql, function (result) { pg2.query(sql1, function (result) { }); });
+
+  pg2.query(sql, function (result) {});
   
   Wurl = '/lfcontact/' + req.body.name;
   res.redirect(Wurl);
